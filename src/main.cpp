06333e49d5a85ff61a167323245bb46201e8a5a6
leRyan11 <30085257@cc.peralta.edu>
2026-05-18 18:32:26 -0700

    Implement basic game loop for Bart Snake

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,22 @@
 using namespace std;
 
 int main() {
-   //make the game 
-  //run game etc
+    BartSnakeGame game;
+    game.startGame();
+
+    cout << "Bart Snake" << endl;
+    cout << "Use w/a/s/d to move, r to render, q to quit." << endl;
+
+    char input;
+    while (game.isRunning() && cin >> input) {
+        if (input == 'r') {
+            game.render();
+        } else {
+            game.processInput(input);
+            game.update();
+            game.render();
+        }
+    }
+
     return 0;
 }
