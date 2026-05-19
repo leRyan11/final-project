dad8c7da769a6d7a97ee74a70db8b02d5fa09e24
leRyan11 <30085257@cc.peralta.edu>
2026-05-18 18:30:26 -0700

    Refactor BartSnake class methods and constructor

--- src/BartSnake.h
+++ src/BartSnake.h
@@ -1,30 +1,70 @@
-#ifndef BART_SNAKE_H
-#define BART_SNAKE_H
+#include "BartSnake.h"
 
-#include <vector>
-#include <utility>
+BartSnake::BartSnake() {
+    body.push_back(std::make_pair(0, 0));
+    directionX = 1;
+    directionY = 0;
+}
 
-class BartSnake {
-private:
-    std::vector<std::pair<int, int>> body;
-    int directionX;
-    int directionY;
-    bool growing;
+BartSnake::BartSnake(int startX, int startY) {
+    body.push_back(std::make_pair(startX, startY));
+    directionX = 1;
+    directionY = 0;
+}
 
-public:
-    BartSnake();
-    BartSnake(int startX, int startY);
+void BartSnake::move() {
+    std::pair<int, int> head = body[0];
+    std::pair<int, int> newHead = std::make_pair(head.first + directionX, head.second + directionY);
 
-    void move();
-    void changeDirection(int dx, int dy);
-    void grow();
+    body.insert(body.begin(), newHead);
+    body.pop_back();
+}
 
-    bool hitWall(int width, int height) const;
-    bool hitSelf() const;
-    bool occupiesPosition(int x, int y) const;
+void BartSnake::changeDirection(int dx, int dy) {
+    directionX = dx;
+    directionY = dy;
+}
 
-    std::pair<int, int> getHeadPosition() const;
-    int getLength() const;
-};
+void BartSnake::grow() {
+    body.push_back(body[body.size() - 1]);
+}
 
-#endif
+bool BartSnake::hitWall(int width, int height) const {
+    std::pair<int, int> head = getHeadPosition();
+    return head.first < 0 || head.first >= width || head.second < 0 || head.second >= height;
+}
+
+bool BartSnake::hitSelf() const {
+    std::pair<int, int> head = getHeadPosition();
+
+    for (int i = 1; i < static_cast<int>(body.size()); i++) {
+        if (body[i] == head) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool BartSnake::occupiesPosition(int x, int y) const {
+    for (int i = 0; i < static_cast<int>(body.size()); i++) {
+        if (body[i].first == x && body[i].second == y) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void BartSnake::setHeadPositionForTest(int x, int y) {
+    body.clear();
+    body.push_back(std::make_pair(x, y));
+}
+
+std::pair<int, int> BartSnake::getHeadPosition() const {
+    return body[0];
+}
+
+int BartSnake::getLength() const {
+    return static_cast<int>(body.size());
+}
