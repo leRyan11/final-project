c92751ba36ec9c2bfef7719aa2490c19f8265b96
leRyan11 <30085257@cc.peralta.edu>
2026-05-18 18:29:06 -0700

    Add first collect card test

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,6 +1,26 @@
+#include <cassert>
 #include <iostream>
+#include <string>
+#include "../src/BartSnakeGame.h"
+
+void test_collect_card_normal() {
+    std::cout << "Collect card (normal)... ";
+    BartSnakeGame game;
+    game.startGame();
+
+    game.setSnakeHeadPositionForTest(2, 2);
+    game.setCardPosition(2, 2);
+
+    bool collected = game.collectCardIfReached();
+
+    assert(collected);
+    assert(game.getScore() == 10);
+    assert(game.getSnakeLength() == 2);
+    assert(!(game.getCardX() == 2 && game.getCardY() == 2));
+    std::cout << "PASSED\n";
+}
 
 int main() {
-    std::cout << "Test file" << std::endl;
+    test_collect_card_normal();
     return 0;
 }
